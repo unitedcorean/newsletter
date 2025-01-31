e8b2191e376e4bb8ddfc1953bea2ae8bad18c25e
unitedcorean <unitedcorean@gmail.com>
2025-01-31 15:17:11 +0900

    Update send_emails.py

--- send_emails.py
+++ send_emails.py
@@ -90,7 +90,7 @@ def main():
     today = datetime.now()
     today_kst = today + timedelta(hours=9)
     date_str = today_kst.strftime("%m월 %d일")
-    subject = f"{date_str} 뉴스브리핑"
+    subject = f"{date_str} KETEP 뉴스브리핑"
 
     # 구독자 목록 가져오기
     subscribers = get_subscribers()
