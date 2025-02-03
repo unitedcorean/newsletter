f0c4fdfb5a6f6f776ed0a0d669e4ce280a2f66f2
unitedcorean <unitedcorean@gmail.com>
2025-02-04 07:57:30 +0900

    Update send_emails.py

--- send_emails.py
+++ send_emails.py
@@ -93,7 +93,7 @@ def main():
     subject = f"{date_str} KETEP 뉴스브리핑"
 
     # 구독자 목록 가져오기
-    subscribers = ["kimyh@ketep.re.kr"]
+    subscribers = ["unitedcorean@naver.com"]
     # subscribers = get_subscribers()
     
     if not subscribers:
