9e8859f55e0e57cf8a06ed753e1e4ea1b25809bf
unitedcorean <unitedcorean@gmail.com>
2025-09-26 00:11:23 +0900

    Update newsletter.py

--- newsletter.py
+++ newsletter.py
@@ -148,7 +148,7 @@ class NewsletterGenerator:
 
     
     def append_to_current(self, new_articles):
-        current_path = "data/current.json"
+        current_path = "data/2025-09.json"
         os.makedirs(os.path.dirname(current_path), exist_ok=True)
     
         # 기존 current.json 불러오기
